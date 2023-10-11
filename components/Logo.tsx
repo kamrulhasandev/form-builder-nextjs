22b96cb8c1bf42462562c6913812dfdee4f857b9
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-12 01:40:09 +0600

    prisma schema create

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Logo() {
   return (
-    <Link href={'/'} className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer">Next Form</Link>
+    <Link href={'/'} className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer">NextForm</Link>
   )
 }
 
