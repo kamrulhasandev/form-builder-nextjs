2ab580c61fabe5101b78ec522295b3963f8fd24d
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-14 04:05:28 +0600

    emplement text field on designer form

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import DesignerSidebar from "./DesignerSidebar";
 import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
 import { cn } from "@/lib/utils";
-import { ElementsType, FormElement } from "./FormElements";
+import { ElementsType, FormElement, FormElementInstance } from "./FormElements";
 import useDesigner from "./hooks/useDesigner";
 import { idGenerator } from "@/lib/idGenerator";
 
@@ -27,6 +27,7 @@ function Designer() {
         const newElement = FormElement[type as ElementsType].construct(
           idGenerator()
         )
+        addElement(0, newElement)
         console.log("NEW Element", newElement);
       }
 
@@ -44,7 +45,7 @@ function Designer() {
             droppable.isOver && "ring-1 ring-primary/20"
           )}
         >
-          {!droppable.isOver && (
+          {!droppable.isOver && elements.length === 0 && (
             <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
               Drop here
             </p>
@@ -54,11 +55,25 @@ function Designer() {
               <div className="h-[120px] rounded-md bg-primary/20"></div>
             </div>
           )}
+          {
+            elements.length > 0 && 
+            <div className="flex flex-col  w-full gap-2 p-4">
+              {elements.map(element => (
+                <DesignerElementWrapper key={element.id} element={element}/>
+              ))}
+            </div>
+          }
         </div>
       </div>
       <DesignerSidebar />
     </div>
   );
+}
+
+
+function DesignerElementWrapper({element}: {element: FormElementInstance}){
+  const DesignerElement = FormElement[element.type].designerComponent;
+  return <DesignerElement elementInstance={element}/>
 }
 
 export default Designer;
--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -1,26 +1,57 @@
-'use client'
+"use client";
 
 import { MdTextFields } from "react-icons/md";
-import { ElementsType, FormElement } from "../FormElements"
+import {
+  ElementsType,
+  FormElement,
+  FormElementInstance,
+} from "../FormElements";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 const type: ElementsType = "TextField";
+
+const extraAttributes = {
+  label: "Text field",
+  helperText: "Helper Text",
+  required: false,
+  placeHolder: "Value here",
+};
+
 export const TextFieldFormElement: FormElement = {
+  type,
+  construct: (id: string) => ({
+    id,
     type,
-    construct: (id: string) =>({
-        id, 
-        type, 
-        extraAttributes: {
-            label: 'Text field',
-            helperText: 'Helper Text',
-            required: false,
-            placeHolder: 'Value here'
-        }
-    }),
-    designerBtnElement: {
-        icon: MdTextFields,
-        label: 'Text field'
-    },
-    designerComponent: () => <div>Designer component</div>,
-    formComponent: () => <div>Form component</div>,
-    propertiesComponent: () => <div>Properties component</div>
+    extraAttributes,
+  }),
+  designerBtnElement: {
+    icon: MdTextFields,
+    label: "Text field",
+  },
+  designerComponent: DesignerComponent,
+  formComponent: () => <div>Form component</div>,
+  propertiesComponent: () => <div>Properties component</div>,
+};
+
+type CustomInstance = FormElementInstance & {
+  extraAttributes: typeof extraAttributes;
+};
+
+function DesignerComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeHolder, helperText } = element.extraAttributes;
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label>
+        {label} {required && "*"}
+      </Label>
+      <Input readOnly disabled placeholder={placeHolder} />
+      {helperText && <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>}
+    </div>
+  );
 }
