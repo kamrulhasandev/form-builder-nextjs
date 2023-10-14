abc733c57786333b3eae0e09d7593f7a54c7f5ec
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-15 03:19:01 +0600

    drag and drop problem solve with dnd sensors

--- components/DragOverLayWrapper.tsx
+++ components/DragOverLayWrapper.tsx
@@ -37,7 +37,7 @@ function DragOverLayWrapper() {
     else {
       const DesignerElementComponent =
         FormElement[element.type].designerComponent;
-        node = <div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4 opacity-80">
+        node = <div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4 opacity-80 pointer-events-none">
           <DesignerElementComponent elementInstance={element}/>
         </div>
     }
--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -5,12 +5,24 @@ import PreviewDialogBtn from "./PreviewDialogBtn";
 import SaveFormBtn from "./SaveFormBtn";
 import PublishFormBtn from "./PublishFormBtn";
 import Designer from "./Designer";
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
 import DragOverLayWrapper from "./DragOverLayWrapper";
 
 function FormBuilder({ form }: { form: Form }) {
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10,
+    }
+  })
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5,
+    }
+  })
+  const sensors = useSensors(mouseSensor, touchSensor);
   return (
-    <DndContext>
+    <DndContext sensors={sensors}>
       <main className="flex flex-col w-full">
         <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
           <h2 className="truncate font-medium">
