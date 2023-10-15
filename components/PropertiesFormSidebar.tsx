9671735fc664785f0fe00cf062b9546ac0cf9b25
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-15 23:52:34 +0600

    side bar button

--- components/PropertiesFormSidebar.tsx
+++ components/PropertiesFormSidebar.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import useDesigner from "./hooks/useDesigner";
 import { FormElement } from "./FormElements";
+import { Button } from "./ui/button";
+import { AiOutlineClose } from "react-icons/ai";
 
 function PropertiesFormSidebar() {
-  const { selectedElement } = useDesigner();
+  const { selectedElement, setSelectedElement } = useDesigner();
   if (!selectedElement) return null;
   const PropertiesForm = FormElement[selectedElement.type].formComponent;
-  return <PropertiesForm />;
+  return (
+    <div className="flex flex-col p-2">
+      <div className="flex justify-between items-center">
+        <p className="text-sm text-foreground/70">Element properties</p>
+        <Button
+          size={"icon"}
+          variant={"ghost"}
+          onClick={() => {
+            setSelectedElement(null);
+          }}
+        >
+          <AiOutlineClose />
+        </Button>
+      </div>
+      <PropertiesForm />
+    </div>
+  );
 }
 
 export default PropertiesFormSidebar;
