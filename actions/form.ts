c66963ea0cb3a92265e44e8d3271d0eea29c1171
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-18 00:15:21 +0600

    test form submit successfully

--- actions/form.ts
+++ actions/form.ts
@@ -198,3 +198,20 @@ export async function SubmitForm(formUrl: string, content: string) {
 
   return updatedForm;
 }
+
+export async function GetFormWithSubmissions(id: number) {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  return await prisma.form.findUnique({
+    where: {
+      userId: user.id,
+      id,
+    },
+    include: {
+      FormSubmissions: true,
+    },
+  });
+}
